6acd790c91b8ad5b480e5479d66c80fb7f510c9b
Oladipo Olasemo <dipolash4@gmail.com>
2021-03-31 15:08:27 +0100

    implemented logic for subcomponent selection

--- lib/hl7parser.ex
+++ lib/hl7parser.ex
@@ -32,7 +32,7 @@ defmodule Hl7parser do
       "query type is for : #{inspect(selector)} segments with locator: #{inspect(locator)}"
     )
 
-    ## 3. get lines that start with segment.
+    ## 3. get lines that start with the selector.
     output =
       doc
       |> Enum.map(fn x -> String.split_at(x, 3) end)
@@ -43,13 +43,14 @@ defmodule Hl7parser do
       [] ->
         {:error, :not_found}
 
-      [{_type, datafields}] ->
+      [ {_type, datafields} ] ->
         ## TODO: this should be refactored into a function/module..
         # 4. get fields from datafields - fields are separated with pipes "|"
         fields = String.split(datafields, "|")
 
         Logger.debug("datafields: #{inspect(fields)}")
 
+        Logger.debug( "#{ inspect( Enum.with_index(fields) ) }" )
         # 5. get the field specified in the locator
         locator_parts = parse_locator(locator)
 
@@ -60,17 +61,49 @@ defmodule Hl7parser do
 
         Logger.debug("datafield: #{inspect(datafield)} ")
 
-        # 7. return the component value
-        ## components in datafields are separated with the ^ symbol
+        ## if there is a bracket in the query..
+        case String.match?(params, ~r/\[(.*?)\]/) do
 
-        components = String.split(datafield, "^")
-        Logger.debug("components: #{inspect(components)} ")
+          true ->
+            ## check if there are repititions within this datafield..
+            components = String.split(datafield, "~")
 
-        component = components |> Enum.at(Enum.at(locator_parts, 1) - 1)
+            Logger.debug("components: #{inspect(components)} ")
 
-        Logger.debug("component: #{inspect(component)} ")
+            component = components
+                        |> Enum.at(Enum.at(locator_parts, 1))
 
-        component
+            Logger.debug("component: #{inspect(component)} ")
+
+            ## check for more subcomponents in the component
+            ## if there are still more segments in the query
+
+            if(Enum.count(locator_parts) == 3) do
+
+              subcomponent = String.split(component, "^")
+                              |> Enum.at(Enum.at(locator_parts, 2) - 1)
+
+              Logger.debug("subcomponent: #{inspect(subcomponent)} ")
+
+              subcomponent
+
+            end
+
+          false ->
+            # 7. return the component value
+            ## components in datafields are separated with the ^ symbol
+
+            components = String.split(datafield, "^")
+
+            Logger.debug("components: #{inspect(components)} ")
+
+            component = components
+                        |> Enum.at(Enum.at(locator_parts, 1) - 1)
+
+            Logger.debug("component: #{inspect(component)} ")
+
+            component
+        end
     end
   end
 
